e30a255ce7ae34437c1d42022d16dbd9b7018153
BVmumbaiIst <gaurang.kadam@bureauveritas.com>
2025-11-07 15:08:06 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import language_tool_python
 
 # --- LangChain imports ---
 from langchain_openai import ChatOpenAI
-from langchain_community.chains import LLMChain, RetrievalQA
+from langchain.chains import LLMChain, RetrievalQA
 from langchain_core.prompts import PromptTemplate
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_community.utilities import SQLDatabase
